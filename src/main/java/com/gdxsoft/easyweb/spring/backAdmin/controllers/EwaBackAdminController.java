ac5bf5066597efcb9fd2918460ee46da0cab23bd
Code Guo <guolei@gdxsoft.com>
2021-12-06 18:51:23 +0800

    backadmin controllers

--- src/main/java/com/gdxsoft/easyweb/spring/backAdmin/controllers/EwaBackAdminController.java
+++ src/main/java/com/gdxsoft/easyweb/spring/backAdmin/controllers/EwaBackAdminController.java
@@ -1,6 +1,8 @@
 package com.gdxsoft.easyweb.spring.backAdmin.controllers;
 
 import java.io.IOException;
+import java.util.Arrays;
+import java.util.List;
 
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
@@ -10,6 +12,9 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+import com.gdxsoft.easyweb.utils.UCookies;
+import com.gdxsoft.web.module.HtModBackAdmin;
+import com.gdxsoft.web.module.HtModule;
 
 @Controller
 public class EwaBackAdminController {
@@ -21,13 +26,28 @@ public class EwaBackAdminController {
 	@ResponseBody
 	public String ewaBackAdminIndex(HttpServletRequest request, HttpServletResponse response)
 			throws ServletException, IOException {
-		 return "";
+		HtModule m = HtModBackAdmin.getIntance().getModelIndex();
+		return m.executeHtmlControl(request, response);
 	}
+
 	@RequestMapping({ "/back_admin/login", "/back_admin/login.jsp" })
 	@ResponseBody
 	public String ewaBackAdminLogin(HttpServletRequest request, HttpServletResponse response) {
-		return "";
+		HtModule m = HtModBackAdmin.getIntance().getModelLogin();
+		return m.executeHtmlControl(request, response);
 	}
+	
+	@RequestMapping({ "/back_admin/login_exit", "/back_admin/login_exit.jsp" })
+	@ResponseBody
+	public String ewaBackAdminLoginExit(HttpServletRequest request, HttpServletResponse response) throws IOException {
+		List<String> skipNames = Arrays.asList("APP_LANG","JSESSIONID","EWA_TIMEDIFF");
+		UCookies.clearCookies(request, response, skipNames);
 
-	 
+		response.sendRedirect( request.getContextPath() +"/back_admin/");
+		
+		return null;
+		
+	}
+	
+
 }
