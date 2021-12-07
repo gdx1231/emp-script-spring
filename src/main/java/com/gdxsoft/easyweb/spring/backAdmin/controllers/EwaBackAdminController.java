5a937be1f6dc0d18179bde76fe15921eef8c1a85
Code Guo <guolei@gdxsoft.com>
2021-12-07 18:55:54 +0800

    http

--- src/main/java/com/gdxsoft/easyweb/spring/backAdmin/controllers/EwaBackAdminController.java
+++ src/main/java/com/gdxsoft/easyweb/spring/backAdmin/controllers/EwaBackAdminController.java
@@ -1,8 +1,6 @@
 package com.gdxsoft.easyweb.spring.backAdmin.controllers;
 
 import java.io.IOException;
-import java.util.Arrays;
-import java.util.List;
 
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
@@ -12,14 +10,20 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
-import com.gdxsoft.easyweb.utils.UCookies;
+import com.gdxsoft.web.acl.Login;
 import com.gdxsoft.web.module.HtModBackAdmin;
 import com.gdxsoft.web.module.HtModule;
 
+/**
+ * 元数据管理系统后台，包含菜单主页，登录页面
+ * @author admin
+ *
+ */
 @Controller
 public class EwaBackAdminController {
 
 	/**
+	 * 元数据管理主页
 	 * URL of the define home
 	 */
 	@RequestMapping({ "/back_admin/", "/back_admin/index.jsp" })
@@ -30,24 +34,35 @@ public class EwaBackAdminController {
 		return m.executeHtmlControl(request, response);
 	}
 
+	/**
+	 * 登录页面
+	 * @param request
+	 * @param response
+	 * @return
+	 */
 	@RequestMapping({ "/back_admin/login", "/back_admin/login.jsp" })
 	@ResponseBody
 	public String ewaBackAdminLogin(HttpServletRequest request, HttpServletResponse response) {
 		HtModule m = HtModBackAdmin.getIntance().getModelLogin();
 		return m.executeHtmlControl(request, response);
 	}
-	
+
+	/**
+	 * 退出登录
+	 * @param request
+	 * @param response
+	 * @return
+	 * @throws IOException
+	 */
 	@RequestMapping({ "/back_admin/login_exit", "/back_admin/login_exit.jsp" })
 	@ResponseBody
 	public String ewaBackAdminLoginExit(HttpServletRequest request, HttpServletResponse response) throws IOException {
-		List<String> skipNames = Arrays.asList("APP_LANG","JSESSIONID","EWA_TIMEDIFF");
-		UCookies.clearCookies(request, response, skipNames);
+		Login.clearLoginCredentials(request, response);
+		
+		response.sendRedirect(request.getContextPath() + "/back_admin/");
 
-		response.sendRedirect( request.getContextPath() +"/back_admin/");
-		
 		return null;
-		
+
 	}
-	
 
 }
---
+++ src/main/java/com/gdxsoft/easyweb/spring/business/controllers/BusinessController.java
@@ -0,0 +1,113 @@
+package com.gdxsoft.easyweb.spring.business.controllers;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import com.gdxsoft.easyweb.script.RequestValue;
+import com.gdxsoft.web.acl.Login;
+import com.gdxsoft.web.http.HttpOaFileView;
+import com.gdxsoft.web.http.HttpQRCode;
+import com.gdxsoft.web.module.HtModBusiness;
+import com.gdxsoft.web.module.HtModule;
+
+@Controller
+public class BusinessController {
+	/**
+	 * 商户系统首页
+	 * 
+	 * @param request
+	 * @param response
+	 * @return
+	 */
+	@RequestMapping({ "/business/", "/business/index", "/business/index.jsp" })
+	@ResponseBody
+	public String businessIndex(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		HtModule m = HtModBusiness.getIntance().getModelIndex();
+
+		RequestValue rv = new RequestValue(request);
+		if ("adm_menu.Menu.Modify".equalsIgnoreCase(rv.s("itemName"))) {
+			// 调用菜单列表
+			m.setItemName("adm_menu.Menu.Modify");
+		}
+
+		return m.executeHtmlControl(request, response);
+	}
+
+	/**
+	 * 商户系统登录页面
+	 * 
+	 * @param request
+	 * @param response
+	 * @return
+	 */
+	@RequestMapping({ "/business/login", "/business/login.jsp" })
+	@ResponseBody
+	public String businessLogin(HttpServletRequest request, HttpServletResponse response) {
+		HtModule m = HtModBusiness.getIntance().getModelLogin();
+
+		RequestValue rv = new RequestValue(request);
+		if ("ADM_USER.Frame.SysChangePwd".equalsIgnoreCase(rv.s("itemName"))) {
+			// 修改密码
+			m.setItemName("ADM_USER.Frame.SysChangePwd");
+		}
+
+		return m.executeHtmlControl(request, response);
+	}
+
+	/**
+	 * 商户系统退出登录
+	 * 
+	 * @param request
+	 * @param response
+	 * @return
+	 * @throws IOException
+	 */
+	@RequestMapping({ "/business/login_exit", "/business/login_exit.jsp" })
+	@ResponseBody
+	public String businessLoginExit(HttpServletRequest request, HttpServletResponse response) throws IOException {
+		Login.clearLoginCredentials(request, response);
+
+		response.sendRedirect(request.getContextPath() + "/business/");
+
+		return null;
+
+	}
+
+	/**
+	 * 输出二维码，参数如下：<br>
+	 * msg 二维码信息<br>
+	 * width 二维码宽带和高度，限制为100 - 1000<br>
+	 * logo 附加到二维码中心的图片url，必须是本地上传的图片在UPath.getPATH_UPLOAD()定义的目录里 <br>
+	 * show 显示方式：=base64 返回图片的base64编码，=url图片的网址，=其它，图片的二进制
+	 * 
+	 * @param request
+	 * @param response
+	 * @return
+	 * @throws Exception
+	 */
+	@RequestMapping({ "/business/qrcode", "/business/qrcode.jsp" })
+	@ResponseBody
+	public String qrCode(HttpServletRequest request, HttpServletResponse response) throws Exception {
+		RequestValue rv = new RequestValue(request);
+		if (!Login.isSupplyLogined(rv)) {
+			return "not logined";
+		}
+		HttpQRCode q = new HttpQRCode();
+		return q.response(request, response);
+	}
+	
+	
+	
+	
+	
+	
+	
+}
